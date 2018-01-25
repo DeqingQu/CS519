cdd866237e68e9c0a04ceece5ee3d6d9c7f77d4d
DeqingQu <qu.deqing@gmail.com>
2018-01-24 20:50:41 -0800

    solution for q1

--- hw3/closest_unsorted.py
+++ hw3/closest_unsorted.py
@@ -1,5 +1,44 @@
+from random import *
+
+
+def qselect(k, a):
+    if k < 1 or k > len(a) or a == []:
+        return []
+    else:
+        r_index = randint(0, len(a)-1)
+        a[0], a[r_index] = a[r_index], a[0]
+        pivot = a[0]
+        left = [x for x in a if x < pivot]
+        right = [x for x in a[1:] if x >= pivot]
+        l_left = len(left)
+        if k-1 < l_left:
+            return qselect(k, left)
+        elif k-1 == l_left:
+            return pivot
+        else:
+            return qselect(k-l_left-1, right)
+
+
 def find(a, x, k):
-    return a
+    if k <= 0:
+        return []
+    if k >= len(a):
+        return a
+
+    b = []
+    for n in a:
+        b.append(abs(n - x))
+
+    t = qselect(k, b)
+
+    result = []
+    count = 0
+    for n in a:
+        if abs(n - x) <= t and count < k:
+            result.append(n)
+            count += 1
+
+    return result
 
 
 if __name__=="__main__":
