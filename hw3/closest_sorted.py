876209ddb1f69a3df951dd4a075b88e9f2a9572c
DeqingQu <qu.deqing@gmail.com>
2018-01-24 17:49:04 -0800

    pass all test

--- hw3/closest_sorted.py
+++ hw3/closest_sorted.py
@@ -7,48 +7,51 @@ def find(a, n, k):
     if k >= len(a):
         return a
 
-    j = bisect.bisect(a, n)
+    p = bisect.bisect_left(a, n)
 
-    if j == 0:
+    if p == 0:
         return a[:k]
-    if j == len(a):
+    if p == len(a):
         return a[len(a) - k:]
 
-    i = j-1
-    diff_i = abs(a[i] - n)
-    diff_j = abs(a[j] - n)
-    if diff_i > diff_j:
-        i = j
-        j += 1
+    diff_1 = abs(a[p-1] - n)
+    diff_2 = abs(a[p] - n)
+    if diff_1 <= diff_2:
+        p -= 1
 
+    i = p - 1
+    j = p + 1
     counter = 1
+    result = [a[p]]
     while counter < k:
         if i <= 0:
+            result.append(a[j])
             j += 1
             counter += 1
             continue
         if j >= len(a):
+            result.insert(0, a[i])
             i -= 1
             counter += 1
             continue
+
         diff_i = abs(a[i] - n)
         diff_j = abs(a[j] - n)
-        if diff_i < diff_j:
+        if diff_i <= diff_j:
+            result.insert(0, a[i])
             i -= 1
             counter += 1
         else:
+            result.append(a[j])
             j += 1
             counter += 1
 
-    return a[i:j]
+    return result
 
 
-def main():
-
-    print(find([1,2,3,4,4,7], 1.8, 3))
+if __name__ =="__main__":
+    print(find([1,2,3,4,4,7], 1.2, 3))
     print(find([1,2,3,4,4,7], 6.5, 3))
     print(find([1, 2, 3, 4, 4, 6, 6], 5, 3))
     print(find([1,2,3,4,4,5,6], 4, 5))
-
-
-main()
+    print(find([1,2,3,4,4,6,6], 5, 3))
