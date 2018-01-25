2562cfbd25a9cd8125aa9af7f0f32ec50034637e
DeqingQu <qu.deqing@gmail.com>
2018-01-24 18:25:38 -0800

    framework for q1

---
+++ hw3/closest_unsorted.py
@@ -0,0 +1,8 @@
+def find(a, x, k):
+    return a
+
+
+if __name__=="__main__":
+    print(find([4,1,3,2,7,4], 5.2, 2))
+    print(find([4,1,3,2,7,4], 6.5, 3))
+    print(find([5,3,4,1,6,3], 3.5, 2))
