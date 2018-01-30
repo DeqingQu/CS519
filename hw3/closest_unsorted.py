7daa011ad0537951ae14ed8a7339e20ac26f5ddf
DeqingQu <qu.deqing@gmail.com>
2018-01-30 10:40:19 -0800

    fix bug in cloest_unsorted

--- hw3/closest_unsorted.py
+++ hw3/closest_unsorted.py
@@ -34,7 +34,11 @@ def find(a, x, k):
     result = []
     count = 0
     for n in a:
-        if abs(n - x) <= t and count < k:
+        if abs(n - x) < t and count < k:
+            result.append(n)
+            count += 1
+    for n in a:
+        if abs(n - x) == t and count < k:
             result.append(n)
             count += 1
 
@@ -45,3 +49,4 @@ if __name__=="__main__":
     print(find([4,1,3,2,7,4], 5.2, 2))
     print(find([4,1,3,2,7,4], 6.5, 3))
     print(find([5,3,4,1,6,3], 3.5, 2))
+    print(find([5,5,5,4,5,8,8,9,], 4, 2))
