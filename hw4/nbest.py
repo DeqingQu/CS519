c83b4761a2a304498c6f8af91e91b28bfd66f0de
DeqingQu <qu.deqing@gmail.com>
2018-02-01 20:21:15 -0800

    solution b for q1

--- hw4/nbest.py
+++ hw4/nbest.py
@@ -2,7 +2,6 @@ import random
 
 
 class Pair(object):
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -16,34 +15,46 @@ class Pair(object):
     def __gt__(self, other):
         return self.x + self.y > other.x + other.y or (self.x + self.y == other.x + other.y and self.y > other.y)
 
-def qsort(a):
-    if a == []:
+def qselect(k, a):
+    if k < 1 or k > len(a) or a == []:
         return []
-    pindex = random.randint(0, len(a)-1)
-    a[0], a[pindex] = a[pindex], a[0]
-    pivot = a[0]
-    left = [w for w in a if w < pivot]
-    middle = [w for w in a if w == pivot]
-    right = [w for w in a if w > pivot]
-    return qsort(left) + middle + qsort(right)
-
-def qselect(a, k):
-    return a
+    else:
+        r_index = random.randint(0, len(a)-1)
+        a[0], a[r_index] = a[r_index], a[0]
+        pivot = a[0]
+        left = [x for x in a if x < pivot]
+        l_left = len(left)
+        if k-1 < l_left:
+            return qselect(k, left)
+        elif k-1 == l_left:
+            return pivot
+        else:
+            right = [x for x in a[1:] if x > pivot or x == pivot]
+            return qselect(k-l_left-1, right)
 
 def nbesta(a, b, n):
     product = []
     for va in a:
         for vb in b:
             product.append(Pair(va, vb))
-    sorted_product = qsort(product)
-    return [(w.x, w.y) for w in sorted_product[:n]]
+    product.sort()
+    return [(w.x, w.y) for w in product[:n]]
 
 def nbestb(a, b, n):
     product = []
     for va in a:
         for vb in b:
-            product.append((va, vb))
-    return qselect(product, n)
+            product.append(Pair(va, vb))
+    p = qselect(n, product)
+    res = [(w.x, w.y) for w in product if w < p]
+    i = 0
+    lp = len(product)
+    while len(res) < n and i < lp:
+        w = product[i]
+        if w == p:
+            res.append((w.x, w.y))
+        i += 1
+    return res
 
 def nbestc(a, b, n):
     return ()
