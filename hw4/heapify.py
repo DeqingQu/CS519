abfbf7e13f48d38e19858957deed35052a14225c
DeqingQu <qu.deqing@gmail.com>
2018-02-03 23:57:58 -0800

    sorted and reversed-sorted array for time analysis of heapify and push elements methods

--- hw4/heapify.py
+++ hw4/heapify.py
@@ -1,31 +1,41 @@
 import random
 import heapq
-import copy
 import timeit
 
 def test1():
-    a = list(range(10))
+    a = list(range(1000000))
     random.shuffle(a)
     heapq.heapify(a)
 
 def test2():
-    a = list(range(10))
+    a = list(range(1000000))
     random.shuffle(a)
     heap = []
     for x in a:
         heapq.heappush(heap, x)
 
-if __name__ == '__main__':
+def test1_1():
+    a = list(range(1000000))
+    heapq.heapify(a)
 
+def test2_1():
+    a = list(range(1000000))
+    heap = []
+    for x in a:
+        heapq.heappush(heap, x)
+
+def test1_2():
+    a = list(range(1000000))
+    a.reverse()
+    heapq.heapify(a)
+
+def test2_2():
+    a = list(range(1000000))
+    a.reverse()
+    heap = []
+    for x in a:
+        heapq.heappush(heap, x)
+
+if __name__ == '__main__':
     print(timeit.timeit('test1()', setup="from __main__ import test1"))
     print(timeit.timeit('test2()', setup="from __main__ import test2"))
-
-# def test():
-#     """Stupid test function"""
-#     L = []
-#     for i in range(100):
-#         L.append(i)
-#
-# if __name__ == '__main__':
-#     import timeit
-#     print(timeit.timeit("test()", setup="from __main__ import random"))
