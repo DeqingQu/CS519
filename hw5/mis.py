e35a1968443aa0fbcddbf9e3ca35bb7f94c478ac
DeqingQu <qu.deqing@gmail.com>
2018-02-08 20:00:36 -0800

    add more test case

--- hw5/mis.py
+++ hw5/mis.py
@@ -9,9 +9,11 @@ def max_wis(a):
             x = f(l-1)
             y = f(l-2)
             mis[l] = max(x, y+a[l-1])
-            print("f%:%", l, mis[l])
         return mis[l]
     return f(len(a))
 
 
-print(max_wis([7,8,5]))
+print(max_wis([7, 8, 5]))
+print(max_wis([-1, 8, 10]))
+print(max_wis([]))
+print(max_wis([-5, -1, -4]))
