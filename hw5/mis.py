b8be07ddad52dd460ffffccf5128924fa2d2b18e
DeqingQu <qu.deqing@gmail.com>
2018-02-08 20:12:29 -0800

    top-down and bottom-up version

--- hw5/mis.py
+++ hw5/mis.py
@@ -12,8 +12,18 @@ def max_wis(a):
         return mis[l]
     return f(len(a))
 
+def max_wis2(a):
+    x, y = 0, 0
+    for i in range(0, len(a)):
+        x, y = max(x, y + a[i]), x
+    return x
 
 print(max_wis([7, 8, 5]))
 print(max_wis([-1, 8, 10]))
 print(max_wis([]))
 print(max_wis([-5, -1, -4]))
+
+print(max_wis2([7, 8, 5]))
+print(max_wis2([-1, 8, 10]))
+print(max_wis2([]))
+print(max_wis2([-5, -1, -4]))
