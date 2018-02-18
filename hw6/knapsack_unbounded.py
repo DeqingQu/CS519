f519bcda6c7e32ba124ee94df2b3edc6de2bade4
DeqingQu <qu.deqing@gmail.com>
2018-02-17 19:07:13 -0800

    finish unbounded knapsack - poor performance

--- hw6/knapsack_unbounded.py
+++ hw6/knapsack_unbounded.py
@@ -1,23 +1,30 @@
 
 def best(W, items):
 
-    unbounded = {0: 0}
+    unbounded = {0: (0, [0]*len(items))}
     def best_helper(W):
         if W not in unbounded:
             max = 0
-            for item in items:
-                if W - item[0] >= 0 and best_helper(W - item[0]) + item[1] > max:
-                        max = best_helper(W - item[0]) + item[1]
-            unbounded[W] = max
+            index = -1
+            value = [0]*len(items)
+            for i, item in enumerate(items):
+                if W - item[0] >= 0:
+                    v, a = best_helper(W - item[0])
+                    if v + item[1] > max:
+                        max = v + item[1]
+                        index = i
+                        value = list(a)
+            if index != -1:
+                value[index] += 1
+            unbounded[W] = (max, value)
         return unbounded[W]
-
     return best_helper(W)
 
 
 if __name__ == '__main__':
-    print(best(3, [(2, 4), (3, 5)]))
-    print(best(3, [(1, 5), (1, 5)]))
-    print(best(3, [(1, 2), (1, 5)]))
+    # print(best(3, [(2, 4), (3, 5)]))
+    # print(best(3, [(1, 5), (1, 5)]))
+    # print(best(3, [(1, 2), (1, 5)]))
     print(best(3, [(1, 2), (2, 5)]))
     print(best(58, [(5, 9), (9, 18), (6, 12)]))
     print(best(92, [(8, 9), (9, 10), (10, 12), (5, 6)]))
