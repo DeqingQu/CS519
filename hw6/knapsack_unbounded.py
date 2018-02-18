b41b131c0e24bf7743fa4089b3b9c9ab161e9db7
DeqingQu <qu.deqing@gmail.com>
2018-02-17 19:08:41 -0800

    add more test cases to unbounded knapsack

--- hw6/knapsack_unbounded.py
+++ hw6/knapsack_unbounded.py
@@ -22,9 +22,9 @@ def best(W, items):
 
 
 if __name__ == '__main__':
-    # print(best(3, [(2, 4), (3, 5)]))
-    # print(best(3, [(1, 5), (1, 5)]))
-    # print(best(3, [(1, 2), (1, 5)]))
+    print(best(3, [(2, 4), (3, 5)]))
+    print(best(3, [(1, 5), (1, 5)]))
+    print(best(3, [(1, 2), (1, 5)]))
     print(best(3, [(1, 2), (2, 5)]))
     print(best(58, [(5, 9), (9, 18), (6, 12)]))
     print(best(92, [(8, 9), (9, 10), (10, 12), (5, 6)]))
