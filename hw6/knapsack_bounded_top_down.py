6fb0c68bc9eeb8ce771ede947c3aa3cbbb6a893c
DeqingQu <qu.deqing@gmail.com>
2018-02-19 15:35:41 -0800

    add performance tests

--- hw6/knapsack_bounded_top_down.py
+++ hw6/knapsack_bounded_top_down.py
@@ -34,9 +34,60 @@ def best(W, items):
 
     return best_helper(W, len(items)-1), solution(W, len(items)-1)
 
-if __name__ == '__main__':
-    print(best(3, [(2, 4, 2), (3, 5, 3)]))
-    print(best(3, [(1, 5, 2), (1, 5, 3)]))
-    print(best(3, [(1, 5, 1), (1, 5, 3)]))
-    print(best(20, [(1, 10, 6), (3, 15, 4), (2, 10, 3)]))
-    print(best(92, [(1, 6, 6), (6, 15, 7), (8, 9, 8), (2, 4, 7), (2, 20, 2)]))
+#   Bottom Up
+
+def best2(weight, item):
+    opt = [[0 for _ in range(weight+1)] for _ in range(len(item)+1)]  # initialize results matrix
+
+    back = [[0 for _ in range(weight+1)] for _ in range(len(item))]
+
+    for i, (w, v, c) in enumerate(item):
+        i += 1
+        for x in range(1, weight+1):
+            for j in range(min(c, x//w)+1):
+                if x >= j*w and opt[i][x] < opt[i-1][x-j*w] + j * v:
+                    opt[i][x] = opt[i-1][x-j*w] + j * v
+                    back[i-1][x] = j
+
+    def solution(w, i):
+        if i < 0:
+            return []
+        new_w = w - item[i][0] * back[i][w]
+        return solution(new_w, i-1) + [back[i][w]]
+
+    return opt[len(item)][weight], solution(weight, len(item)-1)
+
+#   Top-down test
+print("Top-down test")
+print(best(3, [(2, 4, 2), (3, 5, 3)]))
+print(best(3, [(1, 5, 2), (1, 5, 3)]))
+print(best(3, [(1, 5, 1), (1, 5, 3)]))
+print(best(20, [(1, 10, 6), (3, 15, 4), (2, 10, 3)]))
+print(best(92, [(1, 6, 6), (6, 15, 7), (8, 9, 8), (2, 4, 7), (2, 20, 2)]))
+#   Bottom-up test
+print("Bottom-up test")
+print(best2(3, [(2, 4, 2), (3, 5, 3)]))
+print(best2(3, [(1, 5, 2), (1, 5, 3)]))
+print(best2(3, [(1, 5, 1), (1, 5, 3)]))
+print(best2(20, [(1, 10, 6), (3, 15, 4), (2, 10, 3)]))
+print(best2(92, [(1, 6, 6), (6, 15, 7), (8, 9, 8), (2, 4, 7), (2, 20, 2)]))
+
+import sys
+sys.setrecursionlimit(1000000)
+
+import random
+times = 200
+lst = []
+for _ in range(times):
+    lst.append((random.randint(1, 10), random.randint(5, 20), random.randint(1, 10)))
+Weight = random.randint(times*10, times*11)
+
+print("weight = ", Weight)
+
+from time import time
+t= time()
+a = best(Weight, lst)
+print("Top-Down Time : ", time() -t)
+t= time()
+a = best2(Weight, lst)
+print("Bottom-Up Time : ", time() -t)
