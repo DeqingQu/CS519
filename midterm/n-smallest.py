6937bb95a63ce74be1e9e4e65bc2f47f82a7e1c5
DeqingQu <qu.deqing@gmail.com>
2018-02-20 17:47:48 -0800

    add more test cases

--- midterm/n-smallest.py
+++ midterm/n-smallest.py
@@ -9,8 +9,10 @@ def print3DMatrix(m):
             print(a)
         print("---")
     print("]")
-
+    print("------")
 
 if __name__ == '__main__':
-    m = [[[1, 2], [3, 4]], [[2, 3], [4, 5]]]
-    print3DMatrix(m)
+    testcase1 = [[[1, 2], [3, 4]], [[2, 3], [4, 5]]]
+    testcase2 = [[[1, 2, 3], [2, 3, 4], [3, 4, 5]], [[2, 3, 4], [3, 4, 5], [4, 5, 6]], [[3,4,5], [4,5,6], [5,6,7]]]
+    print3DMatrix(testcase1)
+    print3DMatrix(testcase2)
