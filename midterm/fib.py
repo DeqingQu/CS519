4866ac3f7d48bd7836349982d3dec8a7ed8fadbe
DeqingQu <qu.deqing@gmail.com>
2018-02-20 17:36:16 -0800

    python parameter problem

--- midterm/fib.py
+++ midterm/fib.py
@@ -4,7 +4,16 @@ def fib(n, fibs={1:1, 2:1}):
         print(n)
     return fibs[n]
 
+def fib2(n, fibs=None):
+    if fibs is None:
+        fibs = {1:1, 2:1}
+    if n not in fibs:
+        fibs[n] = fib2(n-2, fibs) + fib2(n-1, fibs)
+        print(n)
+    return fibs[n]
 
 if __name__ == '__main__':
     print("# fib(10) = ", fib(10))
     print("# fib(20) = ", fib(20))
+    print("# fib2(10) = ", fib2(10))
+    print("# fib2(20) = ", fib2(20))
