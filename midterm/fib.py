ca5cf668487270bb6ce5b9f218539632544453d5
DeqingQu <qu.deqing@gmail.com>
2018-02-22 22:31:24 -0800

    fib loop version

--- midterm/fib.py
+++ midterm/fib.py
@@ -12,8 +12,15 @@ def fib2(n, fibs=None):
         print(n)
     return fibs[n]
 
+def fib3(n):
+    a,b = 1,1
+    for _ in range(3,n+1):
+        a,b = b,a+b
+    return b
+
 if __name__ == '__main__':
     print("# fib(10) = ", fib(10))
     print("# fib(20) = ", fib(20))
     print("# fib2(10) = ", fib2(10))
     print("# fib2(20) = ", fib2(20))
+    print("# fib3(20) = ", fib3(20))
