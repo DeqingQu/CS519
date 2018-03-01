b903c4d0372672562225aea394e73741c25b51a9
DeqingQu <qu.deqing@gmail.com>
2018-03-01 14:21:43 -0800

    lis solution with O(n^2)

--- hw8/lis.py
+++ hw8/lis.py
@@ -1,11 +1,16 @@
 def lis(s):
-    l = len(s)
-    if l == 1: return s
-    pre = lis(s[0:l-1])
-    if pre[len(pre)-1] < s[l-1]:
-        return pre + s[l-1]
-    else:
-        return pre
+    l, arr = len(s), []
+    arr.append((0, s[0]))
+    for i in range(1, l):
+        max_lis = -1
+        max_index = -1
+        for j in range(i):
+            if s[j] < s[i] and arr[j][0] > max_lis:
+                max_lis = arr[j][0]
+                max_index = j
+        if max_index == -1: arr.append((0, s[i]))
+        else: arr.append((max_lis + 1,  arr[max_index][1] + s[i]))
+    return arr[l-1][1]
 
 if __name__ == '__main__':
     print(lis("aebbcg"))
