5d7443e7755f7c50ff9dfc7bfc3b0600cad2a954
DeqingQu <qu.deqing@gmail.com>
2018-03-02 15:48:20 -0800

    poor performance solution

--- hw8/viterbi.py
+++ hw8/viterbi.py
@@ -29,7 +29,33 @@ def order(n, edges):
     return topol_order
 
 def longest(n, edges):
-    return edges
+
+    in_degree = [0]*n
+    dilg = [0]*n
+    back = [0]*n
+    for (v, u) in edges:
+        in_degree[u] += 1
+    topol_order = order(n, edges)
+    for i in topol_order:
+        if in_degree[i] != 0:
+            for (v, u) in edges:
+                if u == i:
+                    if dilg[i] < dilg[v] + 1:
+                        dilg[i] = dilg[v] + 1
+                        back[i] = v
+    max_dilg, v = 0, 0
+    for i, w in enumerate(dilg):
+        if w > max_dilg:
+            max_dilg = w
+            v = i
+
+    path = []
+    path.append(v)
+    while in_degree[v] != 0:
+        v = back[v]
+        path[:0] = [v]
+
+    return max_dilg, path
 
 if __name__ == '__main__':
     print(longest(8, [(0, 2), (1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 5), (5, 6), (5, 7)]))
