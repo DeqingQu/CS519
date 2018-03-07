fcd22485fcbaad3f93be7e53454ddd0104d80da5
DeqingQu <qu.deqing@gmail.com>
2018-03-07 10:17:43 -0800

    save edges to dic

--- hw9/dijkstra.py
+++ hw9/dijkstra.py
@@ -1,5 +1,13 @@
+from collections import defaultdict
+
 def shortest(n, edges):
-    return n, edges
+
+    graph = defaultdict(list)
+    for (u, v, w) in edges:
+        graph[u].append((v, w))
+        graph[v].append((u, w))
+
+    return graph
 
 if __name__ == '__main__':
     print(shortest(4, [(0, 1, 1), (0, 2, 5), (1, 2, 1), (2, 3, 2), (1, 3, 6)]))
