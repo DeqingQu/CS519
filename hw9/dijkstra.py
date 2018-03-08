e9cd1bdbcd4ee71393bb378c824ba37f4e18442f
DeqingQu <qu.deqing@gmail.com>
2018-03-08 07:50:43 -0800

    while true

--- hw9/dijkstra.py
+++ hw9/dijkstra.py
@@ -2,7 +2,6 @@ from collections import defaultdict
 from heapdict import heapdict
 
 def shortest(n, edges):
-
     graph = defaultdict(list)
     for (u, v, w) in edges:
         graph[u].append((v, w))
@@ -15,7 +14,7 @@ def shortest(n, edges):
 
     length = 0
     back = defaultdict(int)
-    while len(hd) != 0:
+    while True:
         v, w = hd.popitem()
         length = w
         if v == n-1: break
@@ -34,11 +33,43 @@ def shortest(n, edges):
 
     return length, path
 
+def shortest2(n, edges):
+    graph = defaultdict(list)
+    for (u, v, w) in edges:
+        graph[u].append((v, w))
+        graph[v].append((u, w))
+
+    hd = heapdict()
+    hd[0] = 0
+
+    trash = []
+    back = defaultdict(int)
+    while True:
+        v, w = hd.popitem()
+        trash.append(v)
+        length = w
+        if v == n-1: break
+        for (vv, ww) in graph[v]:
+            if vv not in trash:
+                if vv not in hd or hd[vv] > w + ww:
+                    hd[vv] = w + ww
+                    back[vv] = v
+
+    #  backtrack the shortest path
+    path, d = [], n-1
+    path.append(n-1)
+    path[:0] = [back[d]]
+    while back[d] != 0:
+        d = back[d]
+        path[:0] = [back[d]]
+
+    return length, path
+
 
 if __name__ == '__main__':
-    print(shortest(4, [(0, 1, 1), (0, 2, 5), (1, 2, 1), (2, 3, 2), (1, 3, 6)]))
-    print(shortest(5, [(0, 1, 3), (0, 2, 1), (1, 3, 2), (2, 4, 1)]))
-    print(shortest(4, [(0, 1, 24), (0, 2, 3), (0, 3, 20), (2, 3, 12)]))
+    print(shortest2(4, [(0, 1, 1), (0, 2, 5), (1, 2, 1), (2, 3, 2), (1, 3, 6)]))
+    print(shortest2(5, [(0, 1, 3), (0, 2, 1), (1, 3, 2), (2, 4, 1)]))
+    print(shortest2(4, [(0, 1, 24), (0, 2, 3), (0, 3, 20), (2, 3, 12)]))
 
 
     def generate_seq(k, length, seed):
@@ -54,7 +85,7 @@ if __name__ == '__main__':
         shortest(5000, tuples_1[:50000])
         print("test case 1 : ", time() - t)
         t = time()
-        shortest(5000, tuples_2[:50000])
+        shortest2(5000, tuples_1[:50000])
         print("test case 2 : ", time() - t)
 
     performance_test()
