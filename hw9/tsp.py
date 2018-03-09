b9679834403cbf03d99c27e8f41a490341c151e6
DeqingQu <qu.deqing@gmail.com>
2018-03-08 20:19:45 -0800

    tran-bit

--- hw9/tsp.py
+++ hw9/tsp.py
@@ -2,6 +2,19 @@ from collections import defaultdict
 import copy
 from time import time
 
+def trans_bit(n, array):
+    res = 0
+    for i in range(n):
+        if i in array:
+            res = (res<<1) + 1
+        else:
+            res = res<<1
+    if -1 in array:
+        res = (res<<1) + 1
+    else:
+        res = res<<1
+    return res
+
 def tsp(n, edges):
 
     graph = defaultdict(dict)
@@ -15,7 +28,12 @@ def tsp(n, edges):
             graph[u][-1] = w
             graph[-1][u] = w
 
+    opt = defaultdict(dict)
+
     def tsp_helper(visited, i):
+        bit = trans_bit(n, visited)
+        if opt[bit][i]:
+            return opt[visited][i]
         if visited == {0} and i == 0:
             return 0
         min_cost = float("inf")
@@ -26,21 +44,24 @@ def tsp(n, edges):
                 temp = tsp_helper(new_visited, v)
                 if temp is not None and min_cost > temp + graph[v][i]:
                     min_cost = temp + graph[v][i]
+
         if min_cost == float("inf"):
-            return None
+            opt[bit][i] = None
         else:
-            return min_cost
+            opt[bit][i] = min_cost
+        return opt[bit][i]
 
-    v_set = set()
+    vertices = []
     for i in range(n):
-        v_set.add(i)
-    v_set.add(-1)
+        vertices.append(i)
+    vertices.append(-1)
 
-    return tsp_helper(v_set, -1)
+    return tsp_helper(vertices, -1)
 
 if __name__ == '__main__':
+
     print(tsp(4, [(0, 1, 1), (0, 2, 5), (1, 2, 1), (2, 3, 2), (1, 3, 6)]))
-    print(tsp(4, [(0, 1, 1), (0, 2, 5), (1, 2, 1), (2, 3, 2), (1, 3, 6), (3, 0, 1)]))
+    # print(tsp(4, [(0, 1, 1), (0, 2, 5), (1, 2, 1), (2, 3, 2), (1, 3, 6), (3, 0, 1)]))
     # print(tsp(11, [(0,1,29),(0,2,20),(0,3,21),(0,4,16),(0,5,31),(0,6,100),(0,7,12),(0,8,4),(0,9,31),(0,10,18),
     #             (1,2,15),(1,3,29),(1,4,28),(1,5,40),(1,6,72),(1,7,21),(1,8,29),(1,9,41),(1,10,12),
     #             (2,3,15),(2,4,14),(2,5,25),(2,6,81),(2,7,9),(2,8,23),(2,9,27),(2,10,13),
