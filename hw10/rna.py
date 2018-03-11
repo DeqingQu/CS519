2af36536ff27429e5e8660d4d3e416a6cb15f282
DeqingQu <qu.deqing@gmail.com>
2018-03-11 13:44:10 -0700

    first version of 1-best

--- hw10/rna.py
+++ hw10/rna.py
@@ -9,8 +9,38 @@ def best(sequence):
         else:
             l = len(sequence)
             sub_sequence = sequence[:l-1]
-            n, res = best(sub_sequence)
-            opt[sequence] = (n+1, res+'.')
+            n, s = best(sub_sequence)
+            res = list(s)
+            stack = []
+            increase = False
+            for i in range(l-2, -1, -1):
+                if res[i] == ')':
+                    stack.append(')')
+                elif res[i] == '(':
+                    stack.pop()
+                if res[i] == '.' and len(stack) == 0:
+                    if sequence[l-1] == 'U' and (sequence[i] == 'A' or sequence[i] == 'G'):
+                        res[i] = '('
+                        increase = True
+                        break
+                    if sequence[l-1] == 'G' and (sequence[i] == 'C' or sequence[i] == 'U'):
+                        res[i] = '('
+                        increase = True
+                        break
+                    if sequence[l-1] == 'U' and sequence[i] == 'G':
+                        res[i] = '('
+                        increase = True
+                        break
+                    if sequence[l-1] == 'A' and sequence[i] == 'U':
+                        res[i] = '('
+                        increase = True
+                        break
+            if increase:
+                res.append(')')
+                opt[sequence] = (n + 1, "".join(res))
+            else:
+                res.append('.')
+                opt[sequence] = (n, "".join(res))
     return opt[sequence]
 
 
@@ -24,6 +54,7 @@ if __name__ == '__main__':
 
     print(best("ACAGU"))
     print(best("UUCAGGA"))
+    print(best("GUUAGAGUCU"))
 
     print(total("ACAGU"))
 
